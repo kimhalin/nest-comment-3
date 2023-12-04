c97a54210ca8c9569ea0908afb424455033cd1b0
HaLin Kim <halin130@tukorea.ac.kr>
2023-12-05 00:25:10 +0900

    chore: swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   initializeTransactionalContext();
 
   const app = await NestFactory.create(AppModule);
-  // TODO: 프로그램 구현
+
+  // ---------------------------- Swagger 설정 ----------------------------
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('어쩌다 Nest 과제 API')
+    .setVersion('1.0')
+    .build();
+  const swaggerOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
+
+  // ---------------------------- Global Pipe 설정 ----------------------------
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(process.env.PORT || 8000);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
